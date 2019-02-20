725ea235cefbcf7148505219f3a4ccf78f762f66
Sunday7777777 <1194185701@qq.com>
2019-02-21 00:40:57 +0800

    注释掉规范警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,10 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "no-tabs":"off",
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-tabs': 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
